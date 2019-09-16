c492cf0de056acfbf3296d6126e82a0ffcb60773
Alexey Khristoforov <farawayfu@gmail.com>
2019-09-16 18:53:16 +0300

    Update test_runtest.py

--- students/test_runtest.py
+++ students/test_runtest.py
@@ -1,17 +1,22 @@
+# -*- coding: utf-8 -*-
 
+import os
 import runtests
 
 
 def test_path_select():
     """Tests for 'path_select' function."""
-    assert runtests.path_select("D:\\univ\\tests\\homework1") == True
-    assert runtests.path_select("D:\\univ\\tests") == True
+    if not (runtests.path_select('D:\\univ\\tests\\homework1')):
+        raise AssertionError()
+    if not (runtests.path_select('D:\\univ\\tests')):
+        raise AssertionError()
 
 
 def test_file_select():
     """Tests for 'file_select' function."""
-    assert runtests.file_select("D:\\univ\\tests\\homework1", "test_file.py") == True
-    assert runtests.file_select("D:\\univ\\tests\\homework1", "test_") == True
+
+    if not (runtests.file_select('D:\\univ\\tests\\homework1', 'test_file.py')):
+        raise AssertionError()
 
 
 if __name__ == '__main__':
