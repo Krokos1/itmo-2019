60ed91b2341e5530e43c830236048323037b7d22
Alexey Khristoforov <farawayfu@gmail.com>
2019-09-16 18:40:11 +0300

    Update test_file.py

--- students/test_file.py
+++ students/test_file.py
@@ -7,7 +7,7 @@ def test_sum():
     """Tests for my_sum funtion."""
     assert funcs1.my_sum(1, 2) == 3
     assert funcs1.my_sum(5, 2) == 7
-    assert funcs1.my_sum(20, 30) == 50
+    assert funcs1.my_sum(0, 0) == 0
 
 
 def test_mult():
@@ -20,7 +20,7 @@ def test_div():
     """Tests for my_div funtion."""
     assert funcs1.my_div(1, 1) == 1
     assert funcs1.my_div(6, 3) == 2
-    assert funcs1.my_div(100, 5) == 20
+    assert funcs1.my_div(8, 1) == 8
 
 
 if __name__ == '__main__':
