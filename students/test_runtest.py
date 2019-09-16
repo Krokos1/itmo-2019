6f04cb66d965a64031ffc957a2d43ecb16562faa
Alexey Khristoforov <farawayfu@gmail.com>
2019-09-16 19:09:59 +0300

    Update test_runtest.py

--- students/test_runtest.py
+++ students/test_runtest.py
@@ -5,17 +5,17 @@ import runtests
 
 def test_path_select():
     """Tests for 'path_select' function."""
-    str1 = 'D:\\univ\\tests\\homework1'
+    str1 = r'D:\\univ\\tests\\homework1'
     if not (runtests.path_select(str1)):
         raise AssertionError()
-    str1 = 'D:\\univ\\tests'
+    str1 = r'D:\\univ\\tests'
     if not (runtests.path_select(str1)):
         raise AssertionError()
 
 
 def test_file_select():
     """Tests for 'file_select' function."""
-    str1 = 'D:\\univ\\tests\\homework1'
+    str1 = r'D:\\univ\\tests\\homework1'
     if not (runtests.file_select(str1, 'test_file.py')):
         raise AssertionError()
 
