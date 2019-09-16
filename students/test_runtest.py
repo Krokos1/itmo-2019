e3f54b836ca3d0364207ea5c5240474b9db3519c
Alexey Khristoforov <farawayfu@gmail.com>
2019-09-16 17:28:49 +0300

    Update test_runtest.py

--- students/test_runtest.py
+++ students/test_runtest.py
@@ -3,13 +3,13 @@ import runtests
 
 
 def test_path_select():
-    """ Tests for 'path_select' function """
+    """Tests for 'path_select' function."""
     assert runtests.path_select("D:\\univ\\tests\\homework1") == True
     assert runtests.path_select("D:\\univ\\tests") == True
 
 
 def test_file_select():
-    """ Tests for 'file_select' function """
+    """Tests for 'file_select' function."""
     assert runtests.file_select("D:\\univ\\tests\\homework1", "test_file.py") == True
     assert runtests.file_select("D:\\univ\\tests\\homework1", "test_") == True
 
