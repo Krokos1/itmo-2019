71052d8ffc804b871bceb990d4244113bc748de0
Alexey Khristoforov <farawayfu@gmail.com>
2019-09-16 17:16:11 +0300

    Update funcs1.py

--- students/funcs1.py
+++ students/funcs1.py
@@ -1,13 +1,13 @@
 def my_sum(first, second):
-	""" Returns summ """
-	return first + second
+    """ Returns summ """
+    return first + second
 
 
 def my_mult(first, second):
-	""" Returns mult """
-	return first * second
+    """ Returns mult """
+    return first * second
 
 
 def my_div(first,second):
-	""" Returns div """
-	return first / second
+    """ Returns div """
+    return first / second
