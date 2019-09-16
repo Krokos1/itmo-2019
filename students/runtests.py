7df0d0fb4ce321c395575a0278c4e204761be339
Alexey Khristoforov <farawayfu@gmail.com>
2019-09-16 19:28:59 +0300

    Update runtests.py

--- students/runtests.py
+++ students/runtests.py
@@ -1,39 +1,41 @@
+# -*- coding: utf-8 -*-
+
 import os
-from pathlib import Path
+import pathlib
 import sys
 import traceback
 import inspect
 
 
 def path_select(dir_name):
-    """Returns 'True' dir exists on a computer."""
+    """ Returns 'True' dir exists on a computer """
     return os.path.isdir(dir_name)
 
 
-def file_select(dir_name, file):
-    """Returns 'True' if file has '.py' extention and starts with 'test_'."""
-    if (Path(dir_name + "/" + file).suffix == '.py' and file[0:5] == "test_"):
+def file_select(dir_name, file1):
+    """ Returns 'True' if file has '.py' extention and starts with 'test_' """
+    if (pathlib.Path(dir_name + '/' + file1).suffix == '.py' and file1[0:5] == 'test_'):
         return True
 
 
 if __name__ == '__main__':
     while True:
-        dir_name = input("Enter your path: ")
+        dir_name = input('Enter your path: ')  #noqa: WPS421, S322, E501
         print(dir_name)
         if path_select(dir_name):
             sys.path.append(dir_name)
             break
-        elif (dir_name == ""):
+        elif (dir_name == ''):
             dir_name = os.getcwd()
             break
         else:
-            print("Enter valid path")
+            print('Enter valid path')
     file_list = os.listdir(dir_name)
-    for file in file_list:
-        if (file_select(dir_name,file)):
-            func_list = __import__(file[0:-3])
+    for file1 in file_list:
+        if (file_select(dir_name, file1)):
+            func_list = __import__(file1[0:-3])
             for i in range(len(dir(func_list))):
-                if (dir(func_list)[i][0] != "_"):
+                if (dir(func_list)[i][0] != '_'):
                     f = getattr(func_list, dir(func_list)[i])
                     if (inspect.ismodule(f)):
                         __import__(dir(func_list)[i])
@@ -41,12 +43,12 @@ if __name__ == '__main__':
                         try:
                             f()
                         except AssertionError:
-                            print("Test name: " + dir(func_list)[i] + " with path: " + os.getcwd() + "\\" + file 
-                                + " - fail")
+                            print('Test name: ' + dir(func_list)[i] + ' with path: ' + os.getcwd() + '\\' + file 
+                                + ' - fail')  #noqa: T001
                             exc_type, exc_value, exc_traceback = sys.exc_info()
                             traceback.print_exception(exc_type, exc_value, exc_traceback, limit=2, file=sys.stdout)
                         else:
-                            print("Test name: " + dir(func_list)[i] + " with path: " + os.getcwd() + '\\' +file 
-                                + " - ok")
+                            print('Test name: ' + dir(func_list)[i] + ' with path: ' + os.getcwd() + '\\' +file1 
+                                + ' - ok')  #noqa: T001
         else:
-            print(file + " does not meet requirements")
+            print(file1 + ' does not meet requirements')  #noqa: T001
