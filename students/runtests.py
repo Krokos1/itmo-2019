be30cebec457f5191f92b434197e7baa27f3ebea
Alexey Khristoforov <farawayfu@gmail.com>
2019-09-17 16:12:59 +0300

    Update runtests.py

--- students/runtests.py
+++ students/runtests.py
@@ -13,44 +13,41 @@ def path_select(dir_name):
 
 
 def file_select(dir_name, file1):
-    """Returns 'True' if file has .py extention and starts with test_."""
+    """Returns True if file has .py extention and starts with test_."""
     if (pathlib.Path(dir_name + '/' + file1).suffix == 
         '.py' and file1[0:5] == 'test_'):
         return True
 
 
 if __name__ == '__main__':
-    while True:
-        dir_name = input('Enter your path: ')  # noqa: WPS421, S322, E501
-        print(dir_name)
-        if path_select(dir_name):
-            sys.path.append(dir_name)
-            break
-        elif (dir_name == ''):
-            dir_name = os.getcwd()
-            break
-        else:
-            print('Enter valid path')
+
+    dir_name = input('Enter your path: ')  # noqa: WPS421, S322, E501
+    if path_select(dir_name):
+        sys.path.append(dir_name)
+    elif (dir_name == ''):
+        dir_name = os.getcwd()
+    else:
+        print('Enter valid path')  # noqa: T001
+        sys.exit()
     file_list = os.listdir(dir_name)
     for file1 in file_list:
         if (file_select(dir_name, file1)):
             func_list = __import__(file1[0:-3])
-            for i in range(len(dir(func_list))):
-                if (dir(func_list)[i][0] != '_'):
-                    f = getattr(func_list, dir(func_list)[i])
-                    if (inspect.ismodule(f)):
-                        __import__(dir(func_list)[i])
-                    elif (inspect.isfunction(f)):
+            for counter in range(len(dir(func_list))):
+                if (dir(func_list)[counter][0] != '_'):
+                    funk = getattr(func_list, dir(func_list)[counter])
+                    if (inspect.ismodule(funk)):
+                        __import__(dir(func_list)[counter])
+                    elif (inspect.isfunction(funk)):
                         try:
-                            f()
+                            funk()
                         except AssertionError:
-                            print('Test name: ' + dir(func_list)[i] + ' with path: ' 
-                                + os.getcwd() + '\\' + file + ' - fail')  # noqa: T001
-                            exc_type, exc_value, exc_traceback = sys.exc_info()
-                            traceback.print_exception(exc_type, exc_value, exc_traceback,
-                             limit=2, file=sys.stdout)
+                            print('Test name: ' + dir(func_list)[counter] + ' with path: '
+                                + os.getcwd() + '\\' + file1 + ' - fail\n'
+                                + traceback.format_exc())  # noqa: T001
                         else:
-                            print('Test name: ' + dir(func_list)[i] + 
-                                ' with path: ' + os.getcwd() + '\\' +file1 + ' - ok')  # noqa: T001
+                            print('Test name: ' + dir(func_list)[counter] + 
+                                ' with path: ' + os.getcwd() + '\\' +
+                                file1 + ' - ok')  # noqa: T001
         else:
             print(file1 + ' does not meet requirements')  # noqa: T001
